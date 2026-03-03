cee2a6531784d1fb257aa04e7a6b29864f2e2905
Saul Paiva <67033251+saulpaiva@users.noreply.github.com>
2026-03-03 17:29:12 -0300

    Fix input for meaning when word is not found

--- main.py
+++ main.py
@@ -5,12 +5,12 @@ meme_dict = {
 
 word = input("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
 
-significado = input("Digite o significado:")
 
 if word in meme_dict.keys():
     # O que devemos fazer se a palavra for encontrada?
     print("Esta palavra já existe! Tente outra")
 else:
     # O que devemos fazer se a palavra não for encontrada?
+    significado = input("Digite o significado:")
     meme_dict[word] = significado
     print(meme_dict)
